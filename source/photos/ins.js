3e4212212cf3f991e761972e36e0e97896d4cc90
wangdabaoqq <614692339@qq.com>
2017-12-22 23:44:32 +0800

    update

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/wangdabaoqq/hexo/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/wangdabaoqq/hexo/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/wangdabaoqq/hexo/blog/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/wangdabaoqq/hexo/master/blog/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
